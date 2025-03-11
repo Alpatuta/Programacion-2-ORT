5c313ae339b5ad6309de5dbacef93bb8a4dc1704
foteiza <fede.oteiza@gmail.com>
2025-03-11 13:49:04 -0300

    Agregando lo que hicimos

---
+++ Clase1/Clase1/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+
+        public class Persona
+        {
+            private string _nombre;
+            private string _apellido;
+
+            public Persona(string nombre, string apellido)
+            {
+                _nombre = nombre;
+                _apellido = apellido;
+            }
+        }
+    
+}
--- Clase1/Clase1/Program.cs
+++ Clase1/Clase1/Program.cs
@@ -2,10 +2,24 @@
 {
     internal class Program
     {
+        private static Persona[] _personas = new Persona[1];
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hola");
+            AgregarPersona();
+            Console.ReadKey();
+        }
+        static void AgregarPersona()
+        {
+            Console.WriteLine("Ingrese nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese apellido");
+            string apellido = Console.ReadLine();
+            if (nombre.Trim() != "" && apellido.Trim() != "")
+            {
+                Persona persona = new Persona(nombre, apellido);
+                _personas[0] = persona;
+                Console.WriteLine(nombre + "-" + apellido);
+            }
         }
     }
 }
