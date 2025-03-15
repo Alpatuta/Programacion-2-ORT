8c4eea48e5258e8022415ef005af746043c53794
foteiza <fede.oteiza@gmail.com>
2025-03-15 18:37:28 -0300

    Hasta el 3 pronto

--- SolucionPractico1/SolucionPractico1/Program.cs
+++ SolucionPractico1/SolucionPractico1/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Ejercicio1();
+            //Ejercicio1();
             //Ejercicio2();
-            Console.WriteLine("Hello, World!");
+            Ejercicio3();
+            //Ejercicio4();
+        
         }
 
       
@@ -42,7 +44,86 @@
         static void Ejercicio2()
         {
             Console.WriteLine("Ingrese un número");
+            int.TryParse(Console.ReadLine(), out int numeroTabla);
+            int multiplicador = 1;
 
+            while(numeroTabla != 0)
+            {
+                for(int i  = 0; i < 9; i++)
+                {
+                    int resultado = numeroTabla * multiplicador;
+                    Console.WriteLine(numeroTabla + " x " + multiplicador + " = " + resultado);
+                    multiplicador++;
+                }
+
+                multiplicador = 1;
+                Console.WriteLine("Ingrese un número");
+                int.TryParse(Console.ReadLine(), out numeroTabla);
+            }
+
+        }
+
+        // Ejercicio 3
+
+        static void Ejercicio3()
+        {
+            Console.WriteLine("Ingrese el primer numero");
+            int.TryParse(Console.ReadLine(), out int primerNumero);
+
+            Console.WriteLine("Ingrese el segundo numero");
+            int.TryParse(Console.ReadLine(), out int segundoNumero);
+
+            while(primerNumero < segundoNumero && primerNumero != 0 && segundoNumero != 0)
+            {
+                for(int i = primerNumero; i < segundoNumero; i++)
+                {
+                    if(i % 2  == 0)
+                    {
+                        Console.WriteLine(i);
+                        i++;
+                    }
+                }
+
+                Console.WriteLine("Ingrese el primer numero");
+                int.TryParse(Console.ReadLine(), out primerNumero);
+
+                Console.WriteLine("Ingrese el segundo numero");
+                int.TryParse(Console.ReadLine(), out segundoNumero);
+            }
+        }
+
+        // Ejercicio 4
+
+        static void Ejercicio4()
+        {
+            Console.WriteLine("Ingrese el primer numero");
+            int.TryParse(Console.ReadLine(), out int primerNumero);
+
+            Console.WriteLine("Ingrese el segundo numero");
+            int.TryParse(Console.ReadLine(), out int segundoNumero);
+
+            Console.WriteLine("Ingrese un valor");
+            int.TryParse(Console.ReadLine(), out int valor);
+
+            while (primerNumero < segundoNumero && primerNumero != 0)
+            {
+                for(int i =0; i < segundoNumero; i++)
+                {
+                    if (i == valor)
+                    {
+                        Console.WriteLine("El valor esta comprendido entre los numeros");
+                    }
+
+                    if(i != valor)
+                    {
+                        Console.WriteLine("El valor no esta comprendido entre los numeros");
+                    }
+                    
+                }
+
+                Console.WriteLine("Ingrese el primer numero");
+                int.TryParse(Console.ReadLine(), out primerNumero);
+            }
         }
     }
 }
