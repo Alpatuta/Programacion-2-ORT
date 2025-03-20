e04d99e82a4eae594c16baf695223c0e3fb56324
FACULTADES\alumnoFI <alumnoFI>
2025-03-20 10:39:36 -0300

    Terminando el 1

---
+++ Practico-2/Practico-2/Auto.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico_2
+{
+    public class Auto
+    {
+        private string _marca;
+        private string _modelo;
+        private string _matricula;
+        private bool _exoneraImpuestos;
+        private int _anio;
+
+        public string Marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+
+        public string Matricula
+        {
+            get { return _marca; }
+            set { _matricula = value; }
+        }
+
+        public bool ExoneraImpuestos
+        {
+            get { return _exoneraImpuestos; }
+            set { _exoneraImpuestos = value; }
+        }
+
+        public int Anio
+        {
+            get { return _anio; }
+            set { _anio = value; }
+        }
+
+        public Auto (string marca, string modelo, string matricula, bool exoneraImpuestos, int anio)
+        {
+            _marca = marca;
+            _modelo = modelo;
+            _matricula = matricula;
+            _exoneraImpuestos = exoneraImpuestos;
+            _anio = anio;
+        }
+
+        public Auto() { }
+
+        private void ValidarMatricula()
+        {
+            if (_matricula.Trim().Length != 7)
+            {
+                throw new Exception("Matricula invalida: el largo debe ser de 7 digitos");
+            }
+        }
+
+        private void ValidarMarca()
+        {
+            if (_marca.Length < 3)
+            {
+                throw new Exception("Marca invalida: la marca debe tener 3 letras");
+            }
+        }
+
+        public void Validar()
+        {
+            ValidarMatricula();
+            ValidarMarca();
+        } 
+
+        public int CalcularPatente()
+        {
+            int patente = 0;
+
+            if (_anio <= 2015 && _exoneraImpuestos)
+            {
+                patente = 10000;
+            }else if (_anio <= 2015 && !_exoneraImpuestos)
+            {
+                patente = 12000;
+            }
+            else
+            {
+                patente = 17000;
+            }
+
+            return patente;
+        }
+
+        public string DevolverDatos()
+        {
+            return $"Marca: {_marca} \n" + 
+                $"Modelo: {_modelo} \n" + 
+                $"Matricula: {_matricula} \n" + 
+                $"Exonera impuestos: {_exoneraImpuestos} \n" +
+                $"Año: {_anio} \n" + 
+                $"Patente: {CalcularPatente()}";
+        }
+
+
+    }
+}
---
+++ Practico-2/Practico-2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Practico_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicio1();
+        }
+
+        static void Ejercicio1()
+        {
+            Console.WriteLine("Ingrese marca");
+            string marca = Console.ReadLine();
+
+            Console.WriteLine("Ingrese modelo");
+            string modelo = Console.ReadLine();
+
+            Console.WriteLine("Ingrese matricula");
+            string matricula = Console.ReadLine();
+
+            Console.WriteLine("Ingrese si el auto exonera impuestos. Valores validos Si/No");
+            string exonera = Console.ReadLine();
+            bool exoneraImpuestos = false;
+
+            if (exonera == "Si")
+            {
+                exoneraImpuestos = true;
+            }
+
+            Console.WriteLine("Ingrese su año");
+            int.TryParse(Console.ReadLine(), out int anio);
+
+            try
+            {
+                Auto auto = new Auto(marca, modelo, matricula, exoneraImpuestos, anio);
+                auto.Validar();
+                Console.WriteLine(auto.DevolverDatos());
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
