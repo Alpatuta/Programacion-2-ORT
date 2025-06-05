24cc6f564f9f2bbe1bb588c1e4a2b72f2ee841e3
FACULTADES\alumnoFI <alumnoFI>
2025-06-05 08:34:38 -0300

    Guia MVC

---
+++ GuiaMvc/GuiaMvc/Controllers/ParametrosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuiaMvc.Controllers
+{
+    public class ParametrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProbarParametro(int edad)
+        {
+            ViewBag.Mensaje01 = "Recibido el valor" + edad.ToString();
+            return View("Index");
+        }
+
+        public IActionResult ProbarParametroConOtroNombre(int? edad)
+        {
+            if (edad != null)
+            {
+                ViewBag.Mensaje02 = "Recibido el valor " + edad.ToString();
+            }
+            else
+            {
+                ViewBag.Mensaje02 = "No se recibio ningun valor"; 
+            }
+            return View("Index");
+        }
+    }
+}
